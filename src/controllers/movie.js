da238248ffac54e8b50dde6f5104633d1933cc91
Rafael Oviedo <rafaeloviedo@Rafaels-Air.fibertel.com.ar>
2024-05-25 22:56:57 -0300

    update: adding filters to movies endpoint

--- src/controllers/movie.js
+++ src/controllers/movie.js
@@ -2,20 +2,22 @@ const Movies = require('../models/Movie');
 
 const Movie = {
   getAllMovies: async (req, res) => {
-    const category = req.query.category;
+    const { category, name, year, director } = req.query;
 
     try {
-        let query = {};
-        if (category) {
-            query = { category: category };
-        }
+      let movies = Movies.find();
 
-        const movies = await Movies.find(query);
+      if (category) { movies.where('category').equals(category) }
+      if (name) { movies.where('name').regex(new RegExp(name, 'i')) } // Case-insensitive partial match 
+      if (year) { movies.where('year').equals(parseInt(year)) } // Ensure year is an integer
+      if (director) { movies.where('director').regex(new RegExp(director, 'i')) }
 
-        res.status(200).send(movies);
+      const moviesQuery = await movies.exec();
+
+      res.status(200).send(moviesQuery);
 
     } catch (error) {
-        res.status(500).send("Internal Server Error");
+        res.status(500).send({ error: error, message: 'Internal Server Error' });
     }
   },
 
